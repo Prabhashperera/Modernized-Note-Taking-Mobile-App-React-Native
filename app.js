62dcf4fe089b6a81bad10a952eff7ee15ad7f602
Prabhashperera <prabashmperera01@gmail.com>
2026-02-16 18:20:31 +0530

    Navigation With Bars V2 Recreated

---
+++ app.js
@@ -0,0 +1,36 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useContext } from "react";
+import { AuthContext, AuthProvider } from "./src/AuthContext";
+import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <Stack.Screen name="Home" component={HomeScreen} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+}
