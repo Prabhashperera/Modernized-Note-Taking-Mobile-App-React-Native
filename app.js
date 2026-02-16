3ca17198763dc60528dd1104c7edc7ff06a6ef7d
Prabhashperera <prabashmperera01@gmail.com>
2026-02-16 18:51:36 +0530

    Home Screen with CRUD, Without Styles v8

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useContext } from "react";
-import { AuthContext, AuthProvider } from "./src/AuthContext";
+import { AuthContext, AuthProvider } from "./src/Authcontext";
 import HomeScreen from "./src/screens/HomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
